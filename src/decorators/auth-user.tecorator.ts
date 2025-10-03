2ee247d9678c1ddd7d2760c7cc637ad6827cdd8b
Samson96h <samsonhovhannisyan96@gmail.com>
2025-10-03 13:46:23 +0400

    update 4.1

--- src/decorators/auth-user.tecorator.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-
-export const AuthUser = createParamDecorator((field: string, context: ExecutionContext) => {
-    
-    const request = context.switchToHttp().getRequest()
-
-    return field ? request.user?.[field] : request.user;
-})
